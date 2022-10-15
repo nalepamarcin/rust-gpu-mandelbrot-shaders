63136dac73673f8da1b7850123b72436500471b8
Marcin Nalepa <23484467+nalepamarcin@users.noreply.github.com>
2022-10-15 19:55:09 +0200

    Simplify shader with Int8 & Int16 capabilities
    One shader invocation computes only 1 pixel now (previously 4).
    GPU doesn't work well with u16 and u8 in uniform buffer though.

--- src/lib.rs
+++ src/lib.rs
@@ -26,7 +26,7 @@ fn dist_sq(v: glam::Vec2) -> f32 {
 
 // c - coordinates of complex point to check
 // returns 0 if point inside set, otherwise number of iterations (up to max_iter) necessary to escape the set for sure
-fn mandelbrot(c: glam::Vec2, max_iter: u32) -> u32 {
+fn mandelbrot(c: glam::Vec2, max_iter: u8) -> u8 {
     // mandelbrot
     // z_n+1 = z_n * z_n + c
     let mut z = glam::Vec2::new(0.0, 0.0);
@@ -43,7 +43,7 @@ fn mandelbrot(c: glam::Vec2, max_iter: u32) -> u32 {
 }
 
 
-fn mandelmsaax16(c: glam::Vec2, max_iter: u32, img_size: f32) -> u32 {
+fn mandelmsaax16(c: glam::Vec2, max_iter: u8, img_size: f32) -> u8 {
     // uniform distribution of 16 points across pixel
     let dpx = 1.0 / 8.0 / img_size;
     let dpx2 = 3.0 / 8.0 / img_size;
@@ -69,15 +69,15 @@ fn mandelmsaax16(c: glam::Vec2, max_iter: u32, img_size: f32) -> u32 {
         mandelbrot(c + glam::Vec2::new( dpx,  dpx2), max_iter) as u32 +
         mandelbrot(c + glam::Vec2::new( dpx2, dpx2), max_iter) as u32;
 
-    return sum / 16;
+    return (sum / 16u32) as u8;
 }
 
 
-fn mandelproc(c: glam::Vec2, max_iter: u32, img_size: f32) -> u32 {
+fn mandelproc(c: glam::Vec2, max_iter: u8, img_size: f32) -> u8 {
     (mandelmsaax16(c, max_iter, img_size) as f32 / max_iter as f32 * 1.5 * 255.0).clamp(
         0.0,
         255.0
-    ) as u32
+    ) as u8
 }
 
 
@@ -85,27 +85,25 @@ fn mandelproc(c: glam::Vec2, max_iter: u32, img_size: f32) -> u32 {
 pub fn main(
     #[spirv(global_invocation_id)] global_id: glam::UVec3,
     #[spirv(uniform, descriptor_set = 0, binding = 0)] input_parameters: &InputParameters,
-    #[spirv(storage_buffer, descriptor_set = 0, binding = 1)] v_pixels: &mut [u32],
+    #[spirv(storage_buffer, descriptor_set = 0, binding = 1)] v_pixels: &mut [u8],
 ) {
     let img_size_f = input_parameters.img_size_px as f32;
 
-    let get_img_x = |img_x_offset: u32| -> f32 {
-        mix(input_parameters.draw_bounds.x,
-            input_parameters.draw_bounds.y,
-            (global_id.x * 4 + img_x_offset) as f32 / img_size_f
-        )
-    };
-
+    let img_x = mix(
+        input_parameters.draw_bounds.x,
+        input_parameters.draw_bounds.y,
+        global_id.x as f32 / img_size_f
+    );
     let img_y = mix(
         input_parameters.draw_bounds.z,
         input_parameters.draw_bounds.w,
         global_id.y as f32 / img_size_f
     );
 
-    let global_index = global_id.y * (input_parameters.img_size_px / 4) + global_id.x;
-    v_pixels[global_index as usize] =
-        mandelproc(glam::Vec2::new(get_img_x(0), img_y), input_parameters.max_iter, img_size_f)       |
-        mandelproc(glam::Vec2::new(get_img_x(1), img_y), input_parameters.max_iter, img_size_f) << 8  |
-        mandelproc(glam::Vec2::new(get_img_x(2), img_y), input_parameters.max_iter, img_size_f) << 16 |
-        mandelproc(glam::Vec2::new(get_img_x(3), img_y), input_parameters.max_iter, img_size_f) << 24;
+    let global_index = global_id.y * input_parameters.img_size_px + global_id.x;
+    v_pixels[global_index as usize] = mandelproc(
+        glam::Vec2::new(img_x, img_y),
+        input_parameters.max_iter as u8,
+        img_size_f
+    );
 }
